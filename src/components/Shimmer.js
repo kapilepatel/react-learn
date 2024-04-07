21b043dbc6075e5dd8de9fb357513d0e30fe3939
Kapil Patel <kapilepatel@gmail.com>
2024-04-07 16:10:36 +0530

    Shimmer effect withouty any third party package

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,6 +1,8 @@
 const Shimmer =()=>
 {
-    return <h1>Shimmer.....</h1>
+    return <div className="restaurant-list">
+       {Array(10).fill("").map((e, index)=>( <div key={index} className="shimmer-card" ></div>))}
+    </div>
 }
 
 export default Shimmer;
