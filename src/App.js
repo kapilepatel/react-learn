fd2a411745d6a9372e45ebba912def74f098c238
kpatel-potomac <54320123+kpatel-potomac@users.noreply.github.com>
2024-05-05 02:29:36 +0530

    Lazy loaded a new component and used suspense along with fallback

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {lazy, Suspense} from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header"
 import Body from "./components/Body";
 import Footer from "./components/Footer";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"; 
-import About from "./components/About"
+//import About from "./components/About"
 import Error from "./components/Error"
 import Contact from "./components/Contact";
 import RestaurantMenu from "./components/RestaurantMenu";
 import Profile from "./components/Profile";
+import Shimmer from "./components/Shimmer";
+//import Instamart from "./components/Instamart";
+const Instamart = lazy(()=>import("./components/Instamart"));
+const About = lazy(()=> import("./components/about"));
 
 const AppLayout = () => {
   return (
@@ -32,7 +36,7 @@ const appRouter = createBrowserRouter([
       },
       {
         path :"about",
-        element: <About />,
+        element: <Suspense fallback={<h1>Loading...</h1>} > <About /></Suspense>,
         children: [
           {
             path:"profile",
@@ -55,6 +59,13 @@ const appRouter = createBrowserRouter([
   {
     path: "/about",
     element: <About />
+  },
+  {
+    path: "/instamart",
+    element: 
+    <Suspense fallback={<Shimmer />}>
+    <Instamart />
+    </Suspense>
   }
 ]);
 
---
+++ src/components/Instamart.js
@@ -0,0 +1,9 @@
+const Instamart = ()=>{
+    return (
+        <div>
+            <h1>A sample component which we will try to load with lazy load</h1>
+        </div>
+    );
+}
+
+export default Instamart;
